820f68a8fec34caf84f5ebfa7a4444f5e9159709
mirafathila <mirafs04@gmail.com>
2025-10-15 13:18:08 +0700

    Menggunakan Bootstrap

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,15 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="widht=device-widht, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatoble" content="ie=edge">
-    <title>Mahasiswa</title>
+    <title>Home</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 </head>
 <body>
-    <h1>Halaman Mahasiswa</h1>
+    <h1>Halaman Home</h1>
     <ul>
-        <li><a href="/home">Home</a></li>
+        <li>Home</li>
         <li><a href="/about">About</a></li>
-        <li>Mahasiswa</li>
+        <li><a href="/mahasiswa">Mahasiswa</a></li>
         <li><a href="/profil">Profil</a></li>
     </ul>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('index');
+Route::get('home', function () {
+    return view('home');
 });
 
 Route::get('about', function () {
