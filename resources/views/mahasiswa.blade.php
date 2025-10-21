1696225ca194defd9118f5f730c5c7b9b4db4e84
mirafathila <mirafs04@gmail.com>
2025-10-21 23:24:39 +0700

    Belajar Mengenai Tabel

--- resources/views/mahasiswa.blade.php
+++ resources/views/mahasiswa.blade.php
@@ -36,14 +36,41 @@
   </div>
 </nav>
 
-<div class="container" style="margin-left: 30px">
+<div class="container">
     <h1>Ini Adalah Halaman Mahasiswa</h1>
 
-    <div class="row">
-        <div class="col-4" style="background-color: yellow">Halaman A</div>
-        <div class="col-4" style="background-color: blue">Halaman B</div>
-        <div class="col-4" style="background-color: green">Halaman C</div>
-    </div>
+    <table class="table table-danger table-sm table-hover table-striped table-bordered text-center">
+        <thead>
+            <tr>
+                <th>NPM</th>
+                <th>Nama Mahasiswa</th>
+                <th>Jenis Kelamin</th>
+                <th colspan="2">TTL</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>0702233152</td>
+                <td>Mira</td>
+                <td>Perempuan</td>
+                <td>23 Juni 2004</td>
+                <td rowspan="3">Medan</td>
+            </tr>
+            <tr>
+                <td>0702233152</td>
+                <td>Mira</td>
+                <td>Perempuan</td>
+                <td>23 Juni 2004</td>
+            </tr>
+            <tr>
+                <td>0702233152</td>
+                <td>Mira</td>
+                <td>Perempuan</td>
+                <td>23 Juni 2004</td>
+            </tr>
+        </tbody>
+    </table>
+
 </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
